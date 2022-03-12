2b50ace1463949941aa2950115f83361cafd6b42
unknown <luispomare92@gmail.com>
2022-03-12 12:55:52 -0500

    Create js module to read dom elements

--- src/index.js
+++ src/index.js
@@ -1,15 +1,11 @@
-import './style.css';
-
-const taskInput = document.getElementById('taskInput');
-const enterIcon = document.getElementById('enterIcon');
-const ul = document.getElementById('listContainer');
-const clearCompletedButton = document.getElementById('clearCompleted');
+import "./style.css";
+import domObject from "./modules/readDom.js";
 
 let list = [];
 let index = 0;
 
-if (JSON.parse(localStorage.getItem('listArray')) !== null) {
-  list = JSON.parse(localStorage.getItem('listArray'));
+if (JSON.parse(localStorage.getItem("listArray")) !== null) {
+  list = JSON.parse(localStorage.getItem("listArray"));
   index = list.length;
 }
 
@@ -23,15 +19,15 @@ class Item {
 
 // To clear the input field each time the enter button is clicked
 function clearInput() {
-  taskInput.value = '';
+  domObject.taskInput.value = "";
 }
 
 function renderElement(description, index, checked) {
-  let isChecked = '';
-  let checkedClass = '';
+  let isChecked = "";
+  let checkedClass = "";
   if (checked === true) {
-    isChecked = 'checked=true';
-    checkedClass = 'checked';
+    isChecked = "checked=true";
+    checkedClass = "checked";
   }
   const element = `
       <li class="flexItem">
@@ -44,26 +40,26 @@ function renderElement(description, index, checked) {
         </span>
       </li>      
     `;
-  ul.innerHTML += element;
+  domObject.ul.innerHTML += element;
 }
 
 function storageNewElement() {
-  const element = new Item(taskInput.value, false, index);
+  const element = new Item(domObject.taskInput.value, false, index);
   list.push(element);
   index += 1;
-  localStorage.setItem('listArray', JSON.stringify(list));
+  localStorage.setItem("listArray", JSON.stringify(list));
 }
 
-enterIcon.addEventListener('click', () => {
-  if (taskInput.value !== '') {
-    renderElement(taskInput.value, index, false);
+domObject.enterIcon.addEventListener("click", () => {
+  if (domObject.taskInput.value !== "") {
+    renderElement(domObject.taskInput.value, index, false);
     storageNewElement();
     clearInput();
   }
 });
 
 function renderList() {
-  ul.innerHTML = '';
+  domObject.ul.innerHTML = "";
   for (let i = 0; i < list.length; i += 1) {
     renderElement(list[i].description, list[i].index, list[i].completed);
   }
@@ -78,41 +74,41 @@ function clearCompleted() {
   for (let i = 0; i < index; i += 1) {
     list[i].index = i;
   }
-  localStorage.setItem('listArray', JSON.stringify(list));
+  localStorage.setItem("listArray", JSON.stringify(list));
   renderList();
 }
 
-clearCompletedButton.addEventListener('click', () => {
+domObject.clearCompletedButton.addEventListener("click", () => {
   clearCompleted();
 });
 
-ul.addEventListener('click', (e) => {
+domObject.ul.addEventListener("click", (e) => {
   if (e.target) {
-    if (e.target.type === 'checkbox') {
-      e.target.parentElement.classList.toggle('checked');
+    if (e.target.type === "checkbox") {
+      e.target.parentElement.classList.toggle("checked");
       list[e.target.dataset.index].completed = e.target.checked;
-      localStorage.setItem('listArray', JSON.stringify(list));
+      localStorage.setItem("listArray", JSON.stringify(list));
     }
-    if (e.target.tagName === 'I') {
+    if (e.target.tagName === "I") {
       const numericIndex = Number(e.target.dataset.index);
       list = list.filter((item) => item.index !== numericIndex);
       index = list.length;
       for (let i = 0; i < index; i += 1) {
         list[i].index = i;
       }
-      localStorage.setItem('listArray', JSON.stringify(list));
+      localStorage.setItem("listArray", JSON.stringify(list));
       renderList();
     }
-    if (e.target.tagName === 'P') {
+    if (e.target.tagName === "P") {
       e.target.innerHTML = `<input type=text data-index=${e.target.dataset.index} >`;
     }
   }
 });
 
-ul.addEventListener('change', (e) => {
-  if (e.target.type === 'text') {
+domObject.ul.addEventListener("change", (e) => {
+  if (e.target.type === "text") {
     e.target.parentElement.innerHTML = `<p>${e.target.value}</p>`;
     list[e.target.dataset.index].description = e.target.value;
-    localStorage.setItem('listArray', JSON.stringify(list));
+    localStorage.setItem("listArray", JSON.stringify(list));
   }
 });
